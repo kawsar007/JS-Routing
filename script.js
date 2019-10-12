9c5afca41dc158520c81d2394b603165a63f1680
kawsar <imkawsar007@gmail.com>
2019-10-13 00:22:15 +0600

    Routing Project

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// var btn = document.getElementById('btn');
+// var para = document.getElementById('para');
+
+
+// btn.addEventListener('click', function() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'http://localhost:3000/country', true);
+
+//     xhr.onload = function() {
+//         var myData = JSON.parse(xhr.responseText);
+//         renderHTML(myData)
+//     }
+//     xhr.send();
+// })
+
+// function renderHTML(data) {
+//     var htmlString = '';
+//     for (i = 0; i < data.length; i++) {
+//         // htmlString += "<p>" + data[i].name + " is a " + data[i].species + "</p>"
+//         htmlString += "<p>" + "My Country Name is :" + "<b>" + data[i].name + "</b>" + " and Capital :" + data[i].capital + "</p>"
+//     }
+//     // para.insertAdjacentHTML('beforeend', htmlString)
+//     para.innerHTML = htmlString
+}
+
+
+
+
+
 // function Router(path, name) {
 //     this.path = path;
 //     this.name = name;
