b4e53b531769bf4b78b54846923da80cb4c8df26
kawsar <imkawsar007@gmail.com>
2019-10-14 13:26:27 +0600

    project complete

--- data.js
+++ data.js
@@ -1,5 +1,12 @@
+//import axios from "axios";
+
 window.onload = function() {
-    console.log('DOM Has Loaded')
+
+    //console.log('DOM Has Loaded')
+    var initialState = null;
+
+
+
 
     // Create Router Constructor
 
@@ -19,10 +26,12 @@ window.onload = function() {
             view.innerHTML = 'No route exists with this path'
         } else {
             window.history.pushState({}, '', routeInfo.path);
-            console.log(window.history);
+            //console.log(window.history);
+            console.log(initialState);
             view.innerHTML = 'You have clicked the ' + routeInfo.name + ' route'
         }
     };
+
 
     // Add event listeners
     activeRoutes.forEach(function(route) {
@@ -48,11 +57,30 @@ window.onload = function() {
     }, {
         path: '/country',
         name: 'Country'
+    }, {
+        path: '/country/?code',
+        name: 'Country_code'
     }]);
     // console.log(myFirstRouter)
     var currentPath = window.location.pathname;
+    var param = window.location.search.slice(1);
+    //var paramOne = window.location.pathname.split("?")
+    console.log(param);
+    //console.log("paramOne", paramOne);
     if (currentPath === '/') {
+
         view.innerHTML = 'You are on the root page';
+    } else if (param) {
+        if (initialState) {
+            var newData = initialState.filter(item => {
+                return item.code === param
+            })
+            console.log(newData)
+        } else {
+            getData();
+        }
+
+        console.log(initialState);
     } else {
         var route = myFirstRouter.routes.filter(function(r) {
             return r.path === currentPath
@@ -73,17 +101,26 @@ window.onload = function() {
         xhr.onload = function() {
             var myData = JSON.parse(xhr.responseText);
             renderHTML(myData)
+
         }
+
         xhr.send();
+
+
+        //console.log(initialState)
     })
 
+
+
+
     function renderHTML(data) {
+        console.log("My Data", data)
         var htmlString = '';
         for (i = 0; i < data.length; i++) {
             // htmlString += "<p>" + data[i].name + " is a " + data[i].species + "</p>"
             htmlString += "<p>" + "My Country Name is :" + "<b>" + data[i].name + "</b>" + "</p>"
         }
         para.insertAdjacentHTML('beforeend', htmlString)
-        para.innerHTML = htmlString
+        para.innerHTML = htmlString;
     }
 }
